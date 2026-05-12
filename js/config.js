b5771dfa73201ea028b6112289f8e8683f98b964
Jack Begley <jack.begley@supermetrics.com>
2026-05-12 16:27:55 +0100

    v up

--- js/config.js
+++ js/config.js
@@ -15,7 +15,7 @@
 //   - None (Exports constants)
 // ============================================================
 
-export const VERSION = '1.4.15';
+export const VERSION = '1.4.16';
 
 export const REGIONS = {
     WORLD:    { lat: [-60, 70],   lng: [-180, 180], radius: 200000 },
